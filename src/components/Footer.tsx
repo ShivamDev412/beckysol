3afac40d4049b0552f6123ab80810b434a312352
Shivam Singh <shiv41297@gmail.com>
2024-09-20 21:30:15 -0400

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const Footer = () => {
+  return (
+    <footer className="bg-foreground">
+      <Image src="/images/"></Image>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/PreHowToBuy.tsx
+++ src/components/PreHowToBuy.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
+
 const PreHowToBuy = () => {
   return (
-    <div className="w-auto h-[12in] relative">
-        <Image 
-          src="/images/pre-how-to-buy-one.jpeg" 
-          alt="Banner" 
-          layout="fill" 
-          objectFit="cover" 
-          objectPosition="center" 
-          priority
-        />
-      </div>
+    <div className="relative w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[150vh]">
+      <Image 
+        src="/images/pre-how-to-buy-one.jpeg" 
+        alt="Banner" 
+        layout="fill" 
+        objectFit="cover" 
+        objectPosition="center" 
+        priority
+      />
+    </div>
   )
 }
 
