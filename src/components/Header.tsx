ed079191f632399329894200cce1f1e669905c05
Shivam Singh <shiv41297@gmail.com>
2024-09-23 07:24:42 -0400

    Refactor Header component and update navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,12 +23,11 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50 flex justify-between items-center transition-all duration-300 bg-[#93579d]/50 backdrop-blur-lg backdrop-brightness-125 py-2 px-10">
-      {/* Logo */}
       <Image
         src="/images/logo.png"
         alt="becky_logo"
-        width={100}
-        height={100}
+        width={150}
+        height={150}
         className="hover:cursor-pointer"
         onClick={() => scrollToSection("home")}
       />
@@ -41,8 +40,6 @@ const Header = () => {
           <span className="block w-8 h-1 bg-white"></span>
         </button>
       </div>
-
-      {/* Full-screen dropdown menu (Mobile) */}
       {isOpen && (
         <motion.div
           initial={{ y: "-100%", opacity: 0 }}
@@ -51,15 +48,12 @@ const Header = () => {
           transition={{ duration: 0.5 }}
           className="fixed top-0 left-0 w-full bg-[#93579d] z-40 h-screen flex flex-col items-center justify-center space-y-10 text-white"
         >
-          {/* Close button (small white cross) */}
           <button
             onClick={() => setIsOpen(false)}
             className="absolute top-4 right-6 text-white text-4xl font-bold"
           >
             &times;
           </button>
-
-          {/* Logo inside the dropdown */}
           <Image
             src="/images/logo.png"
             alt="becky_logo"
@@ -68,38 +62,40 @@ const Header = () => {
             className="hover:cursor-pointer"
             onClick={() => scrollToSection("home")}
           />
-
-          {/* Navigation Links */}
           <nav className="w-full text-center">
-            <ul className="text-4xl font-bold space-y-8">
-              <li
-                className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
-                onClick={() => scrollToSection("about")}
-              >
-                About
+            <ul className="space-y-8 flex flex-col items-center">
+              <li onClick={() => scrollToSection("about")}>
+                <Image
+                  src={"/images/about-nav.png"}
+                  alt={"about-nav-image"}
+                  height={150}
+                  width={150}
+                />
               </li>
-              <li
-                className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
-                onClick={() => scrollToSection("how-to-buy")}
-              >
-                How To Buy?
+              <li onClick={() => scrollToSection("how-to-buy")}>
+                <Image
+                  src={"/images/how-to-buy-nav.png"}
+                  alt={"how-to-buy-nav-image"}
+                  height={150}
+                  width={150}
+                />
               </li>
-              <li
-                className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
-                onClick={() => scrollToSection("roadmap")}
-              >
-                Roadmap
+              <li onClick={() => scrollToSection("roadmap")}>
+                <Image
+                  src={"/images/roadmap-nav.png"}
+                  alt={"roadmap-nav-image"}
+                  height={150}
+                  width={150}
+                />
               </li>
-              <li
+              {/* <li
                 className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
                 onClick={() => scrollToSection("tokenomics")}
               >
                 Tokenomics
-              </li>
+              </li> */}
             </ul>
           </nav>
-
-          {/* "Buy Now" Button inside the dropdown */}
           <a
             href="https://pump.fun/6aNtorm2HyyxCGuqREXm8BGXnFy9Th5BLmcLENKvpump"
             target="_blank"
@@ -110,38 +106,48 @@ const Header = () => {
           </a>
         </motion.div>
       )}
-
-      {/* Regular Navigation for larger screens */}
       <nav className="hidden md:flex flex-1 justify-center">
         <ul className="flex items-center gap-10 text-3xl font-bold text-white">
           <li
-            className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
+            
             onClick={() => scrollToSection("about")}
           >
-            About
+            <Image
+              src={"/images/about-nav.png"}
+              alt={"about-nav-image"}
+              height={150}
+              width={150}
+            />
           </li>
           <li
-            className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
+           
             onClick={() => scrollToSection("how-to-buy")}
           >
-            How To Buy?
+            <Image
+              src={"/images/how-to-buy-nav.png"}
+              alt={"how-to-buy-nav-image"}
+              height={150}
+              width={150}
+            />
           </li>
           <li
-            className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
             onClick={() => scrollToSection("roadmap")}
           >
-            Roadmap
+            <Image
+              src={"/images/roadmap-nav.png"}
+              alt={"roadmap-nav-image"}
+              height={150}
+              width={150}
+            />
           </li>
-          <li
+          {/* <li
             className="underline hover:cursor-pointer underline-offset-[5.5px] font-[Sofadi One]"
             onClick={() => scrollToSection("tokenomics")}
           >
             Tokenomics
-          </li>
+          </li> */}
         </ul>
       </nav>
-
-      {/* "Buy Now" Button for larger screens */}
       <a
         href="https://pump.fun/6aNtorm2HyyxCGuqREXm8BGXnFy9Th5BLmcLENKvpump"
         target="_blank"
